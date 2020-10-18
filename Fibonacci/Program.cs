65142dc5d336c964ba8d1a1ad8d81cff1583104a
odinokun <odinokun@gmail.com>
2020-10-18 14:39:51 +0300

    did fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Fibonacci(int a, int b)
+        {
+            int result = a + b;
+
+            if (result < 2)
+            {
+                Console.Write(a + ", " + b + ", ");
+            }
+            
+            if (result < 200)
+            {
+                Console.Write(result + ", ");
+                Fibonacci(b, result);
+            }
+
+            //while (result < 200)
+            //{
+            //    Fibonacci(b, result);
+            //}
+
+        }
+
+        static void Main(string[] args)
+        {
+            Fibonacci(0, 1);
+        }
+    }
+}
--- Fibonacci/Properties/AssemblyInfo.cs
+++ Fibonacci/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Loops")]
+[assembly: AssemblyTitle("Fibonacci")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Loops")]
+[assembly: AssemblyProduct("Fibonacci")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6ec52a24-3cc1-408e-accb-43f407b22a5f")]
+[assembly: Guid("e289d9ae-3a5c-486f-a419-c2c0c576a8af")]
 
 // Version information for an assembly consists of the following four values:
 //

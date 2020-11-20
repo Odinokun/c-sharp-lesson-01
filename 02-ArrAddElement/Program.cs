7c5f3b99b10b2f6c5378f944a54933a2a5dce35e
odinokun <odinokun@gmail.com>
2020-11-20 23:01:37 +0200

    binary 50% completed

--- 02-ArrAddElement/Program.cs
+++ 02-ArrAddElement/Program.cs
@@ -11,9 +11,9 @@ namespace _02_ArrAddElement
 
         static void Insert(ref int[] array, int value, int index)
         {
-            int[] newArray = new int[array.Length + 1];
+            int[] newArray = new int[array.Length + 1]; //Создали новый массив с длиной старого + 1
 
-            newArray[index] = value;
+            newArray[index] = value; //Записали в массив новый элемент
 
             for (int i = 0; i < index; i++) //элементы до вставляемого числа
                 newArray[i] = array[i];
@@ -21,7 +21,7 @@ namespace _02_ArrAddElement
             for (int i = index; i < array.Length; i++) //элементы после вставляемого числа
                 newArray[i + 1] = array[i];
 
-            array = newArray;
+            array = newArray; //Заменили старый массив новым
         }
 
         static void Main(string[] args)
@@ -33,9 +33,7 @@ namespace _02_ArrAddElement
                 Console.Write(testArray[i] + ", ");
             }
 
-            Insert(ref testArray, 777, 3);
-
-            Console.WriteLine();
+            Insert(ref testArray, 777, 3); //массив, число которое вставляем, индекс нового числа
 
             for (int i = 0; i < testArray.Length; i++)
             {
---
+++ 06-ArrBinarySearch/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ArrBinarySearch
+{
+    class Program
+    {
+        static void SortArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int k = 0; k < array.Length - 1; k++)
+                {
+                    if (array[k + 1] < array[k])
+                    {
+                        int temp = array[k];
+                        array[k] = array[k + 1];
+                        array[k + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        static int BinarySearch(int[] array, int searchValue, int minValue, int maxValue)
+        {
+            SortArray(array); //сортируем массив 
+
+            if (searchValue > maxValue || searchValue < minValue)
+            {
+                Console.WriteLine("Искомого значения нет в массиве");
+
+                return -1;
+            }
+
+            int middleValue = (minValue + maxValue) / 2; //делим массив пополам
+            int arrMiddleValue = array[middleValue]; //узнаем значение среднего элемента
+
+            if (arrMiddleValue == searchValue) //если значение среднего элемента = искомому числу
+            {
+                Console.WriteLine("arrMiddleValue = " + arrMiddleValue);
+                return arrMiddleValue;
+            } else
+            {
+                if (middleValue > searchValue)
+                {
+                    //ищем в левой половине, рекурсивно вызывая функцию
+                    return BinarySearch(array, searchValue, minValue, middleValue - 1);
+                }
+                else
+                {
+                    //ищем в правой половине, рекурсивно вызывая функцию
+                    return BinarySearch(array, searchValue, middleValue + 1, maxValue);
+                }
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] testArr = { 45, 10, 55, 65, 4, 22, 8, 57, 44, 21, 73, 14, 1, 87, 23, 33, 97 };
+
+            Console.Write("Введите значение которое требуется найти: ");
+            int searchValue = int.Parse(Console.ReadLine());
+
+            BinarySearch(testArr, searchValue, 0, testArr.Length - 1);
+        }
+    }
+}

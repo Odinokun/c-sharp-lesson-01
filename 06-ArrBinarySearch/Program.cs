71df05e1bfff96ba3c49b05aa91e259ff11bbefb
odinokun <odinokun@gmail.com>
2020-11-25 13:48:09 +0200

    did binary search

--- 06-ArrBinarySearch/Program.cs
+++ 06-ArrBinarySearch/Program.cs
@@ -24,39 +24,68 @@ namespace _06_ArrBinarySearch
             }
         }
 
-        static int BinarySearch(int[] array, int searchValue, int minValue, int maxValue)
+        //static int BinarySearch(int[] array, int searchValue, int minValue, int maxValue)
+        //{
+        //    SortArray(array); //сортируем массив 
+
+        //    if (searchValue > maxValue || searchValue < minValue)
+        //    {
+        //        Console.WriteLine("Искомого значения нет в массиве");
+
+        //        return -1;
+        //    }
+
+        //    int middleValue = (minValue + maxValue) / 2; //делим массив пополам
+        //    int arrMiddleValue = array[middleValue]; //узнаем значение среднего элемента
+
+        //    if (arrMiddleValue == searchValue) //если значение среднего элемента = искомому числу
+        //    {
+        //        Console.WriteLine("arrMiddleValue = " + arrMiddleValue);
+        //        return arrMiddleValue;
+        //    } else
+        //    {
+        //        if (middleValue > searchValue)
+        //        {
+        //            ищем в левой половине, рекурсивно вызывая функцию
+        //            return BinarySearch(array, searchValue, minValue, middleValue - 1);
+        //        }
+        //        else
+        //        {
+        //            ищем в правой половине, рекурсивно вызывая функцию
+        //            return BinarySearch(array, searchValue, middleValue + 1, maxValue);
+        //        }
+        //    }
+
+
+        //}
+
+        static int BinarySearch(int[] array, int key)
         {
-            SortArray(array); //сортируем массив 
+            SortArray(array); //сортируем массив
 
-            if (searchValue > maxValue || searchValue < minValue)
+            int minNum = 0;
+            int maxNum = array.Length - 1;
+
+            while (minNum <= maxNum)
             {
-                Console.WriteLine("Искомого значения нет в массиве");
+                int mid = (minNum + maxNum) / 2;
 
-                return -1;
-            }
-
-            int middleValue = (minValue + maxValue) / 2; //делим массив пополам
-            int arrMiddleValue = array[middleValue]; //узнаем значение среднего элемента
-
-            if (arrMiddleValue == searchValue) //если значение среднего элемента = искомому числу
-            {
-                Console.WriteLine("arrMiddleValue = " + arrMiddleValue);
-                return arrMiddleValue;
-            } else
-            {
-                if (middleValue > searchValue)
+                if (key == array[mid])
                 {
-                    //ищем в левой половине, рекурсивно вызывая функцию
-                    return BinarySearch(array, searchValue, minValue, middleValue - 1);
+                    Console.WriteLine("result = " + array[mid]);
+                    return ++mid;
+                }
+                else if (key < array[mid])
+                {
+                    maxNum = mid - 1;
                 }
                 else
                 {
-                    //ищем в правой половине, рекурсивно вызывая функцию
-                    return BinarySearch(array, searchValue, middleValue + 1, maxValue);
+                    minNum = mid + 1;
                 }
             }
-
-
+            Console.WriteLine("Number don`t found");
+            return -1;
         }
 
         static void Main(string[] args)
@@ -66,7 +95,8 @@ namespace _06_ArrBinarySearch
             Console.Write("Введите значение которое требуется найти: ");
             int searchValue = int.Parse(Console.ReadLine());
 
-            BinarySearch(testArr, searchValue, 0, testArr.Length - 1);
+            BinarySearch(testArr, searchValue);
+            //BinarySearch(testArr, searchValue, 0, testArr.Length - 1);
         }
     }
 }
